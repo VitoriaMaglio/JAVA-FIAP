ea9967abe94418e6ef1820bf6eb7e9abab114146
Vitória Valentina Maglio <vitoriamaglio@gmail.com>
2025-04-10 14:10:13 -0300

    Add files via upload

---
+++ ddd-teoria/src/Teoria.java
@@ -0,0 +1,22 @@
+public class Teoria {
+    /*
+    Herança: identifica que tem duas ou mais classes que são o mesmo tipo(Livro físico e ebook = os dois são livros, porém com informações diferentes), porém são dois objetos distintos dentro do sistema com características iguais e diferentes.
+    Reescrever com menos código, tudo que é igual fica na super classe, e nas classes filhas deixa apenas o que é específico
+    Conceito de herança = herda os atributos e características da outra classe
+    SuperClasse tudo que é genérico
+    SubClasse tudo que é específico, herda características, único método que não é herdado é o método construtor
+    Método construtor = diz como o objeto está sendo criado
+    Sobrecarga de método = métdos com mesmo nome porém com assinatura diferentes
+    Sobrescrita de método = mesmo método com mesmo nome e assinatura na SuperClasse e na subclasse
+    vai escrever por cima do método da superclasse, sobrepor conteúdos
+    Modificador de acesso:
+    public todo mundo enxerga ; +
+    private só é visível dentro da própria classe; -
+    default é o padrão, tudo q vc n define são visíveis dentro do pacote
+    protected é utilizado na herança, declarações são visíveis dentro do pacote ou de todas as subclasses
+
+
+
+
+     */
+}
